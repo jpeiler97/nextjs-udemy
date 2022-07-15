e4441fae24937de1e3cc027e37e900c418d3ae00
JP Eiler <jp.eiler97@gmail.com>
2022-07-15 08:07:30 -0500

    added about page

--- sections/001-file-based-routing/file-based-routing/pages/index.js
+++ sections/001-file-based-routing/file-based-routing/pages/index.js
@@ -5,3 +5,11 @@ export default function Home() {
     </div>
   );
 }
+
+/* Notes:
+
+- Next.js sees index.js in the top-level of the pages directory as the root page of the app.
+  Whatever component is in this file will be rendered when a user navigates to "https://[baseURL]/ without
+  any additional paths.
+
+*/

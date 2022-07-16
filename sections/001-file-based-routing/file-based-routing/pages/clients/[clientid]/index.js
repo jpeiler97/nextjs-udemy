f89b9b81976146a7005e825f49bef458fe5f521a
JP Eiler <jp.eiler97@gmail.com>
2022-07-16 08:24:46 -0500

    added programatic route to client page

--- sections/001-file-based-routing/file-based-routing/pages/clients/[clientid]/index.js
+++ sections/001-file-based-routing/file-based-routing/pages/clients/[clientid]/index.js
@@ -1,7 +1,20 @@
+import { useRouter } from "next/router";
+
 export default function ClientProjectsPage() {
+  const router = useRouter();
+
+  function loadProjectHandler() {
+    //blah
+    router.push({
+      pathname: "/clients/[clientid]/[projectid]",
+      query: { clientid: "jp", projectid: "1" },
+    });
+  }
+
   return (
     <div>
       <h1>List of client projects</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
@@ -10,4 +23,7 @@ export default function ClientProjectsPage() {
   - This is just the base page for this dynamic path '[clientid]'. It would be treated the same as creating a file
     named '[clientid].js' on the same level as 'clients/index.js', but the difference here is now it can have another nested
     url path if need be. 
+
+  - Programatic routing can be achieved through router.push(), which accepts a url or an object (see clients/index.js)
+    and will navigate to the pathname like a typical Link component would.
 */

ae5194d6b8513c544e917c4000eb21b22901b099
JP Eiler <jp.eiler97@gmail.com>
2022-07-16 23:11:48 -0500

    added events search filter

--- sections/001-file-based-routing/project-01/pages/events/index.js
+++ sections/001-file-based-routing/project-01/pages/events/index.js
@@ -1,11 +1,17 @@
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/EventList";
+import EventsSearch from "../../components/events/EventsSearch";
+import { useRouter } from "next/router";
 
 export default function Events() {
   const events = getAllEvents();
-
+  const router = useRouter();
+  function handleSearch(year, month) {
+    router.push(`/events/${year}/${month}`);
+  }
   return (
     <div>
+      <EventsSearch handleSearch={handleSearch} />
       <EventList items={events} />
     </div>
   );

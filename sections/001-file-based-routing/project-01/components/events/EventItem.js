23f1c35dd5f71d4bf0eae4990e50eb3bf48b1299
JP Eiler <jp.eiler97@gmail.com>
2022-07-16 22:20:38 -0500

    added styles and copied images from repo

--- sections/001-file-based-routing/project-01/components/events/EventItem.js
+++ sections/001-file-based-routing/project-01/components/events/EventItem.js
@@ -1,6 +1,9 @@
-import Link from "next/link";
+import Button from "../ui/button";
 
 import classes from "./event-item.module.css";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 
 export default function EventItem(props) {
   const { id, title, description, location, date, image, isFeatured } = props;
@@ -19,14 +22,21 @@ export default function EventItem(props) {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{readableDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Link href={`/events/${id}`}>Explore Event</Link>
+          <Button link={`/events/${id}`}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>

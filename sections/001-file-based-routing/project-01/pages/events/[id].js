67deec85f2404de173fbdffdd3bda2858c59a7c1
JP Eiler <jp.eiler97@gmail.com>
2022-07-16 22:31:36 -0500

    copied event components and implented

--- sections/001-file-based-routing/project-01/pages/events/[id].js
+++ sections/001-file-based-routing/project-01/pages/events/[id].js
@@ -1,1 +1,30 @@
-export default function EventPage() {}
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+
+export default function EventPage() {
+  const router = useRouter();
+
+  const event = getEventById(router.query.id);
+
+  if (!event) {
+    return <p>No event found.</p>;
+  }
+
+  return (
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
+  );
+}

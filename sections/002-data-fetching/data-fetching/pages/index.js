7a6dbbe2d83ac210790129e1ad2687d60890a172
JP Eiler <jp.eiler97@gmail.com>
2022-07-18 06:03:06 -0500

    added getStaticProps to index

--- sections/002-data-fetching/data-fetching/pages/index.js
+++ sections/002-data-fetching/data-fetching/pages/index.js
@@ -1,11 +1,32 @@
-function HomePage(props) {
+export default function HomePage(props) {
+  const { products } = props;
+
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map((product) => {
+        return <li key={product.id}>{product.title}</li>;
+      })}
     </ul>
   );
 }
 
-export default HomePage;
+export async function getStaticProps() {
+  return {
+    props: {
+      products: [{ id: "p1", title: "Product1" }],
+    },
+  };
+}
+
+/* Notes:
+
+- When async function getStaticProps() is exported from a page file, Next will see it and understand it
+as a signal to do whatever is inside the function, BEFORE the page is pre-generated. On top of this, if
+the default setting to pre-generate all pages is off, and this file exports getStaticProps(), Next will
+continue to pre-generate the page.
+- The function must always return an object with at least the 'props' key, but whatever data is passed
+in may be anything.
+- The function must be exported/written as export async function getStaticProps() in order for Next to
+detect it.
+
+*/

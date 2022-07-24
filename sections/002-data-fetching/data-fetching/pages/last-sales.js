aef61d88356c2a3febd601df5e1408fd03d5cee9
JP Eiler <jp.eiler97@gmail.com>
2022-07-24 08:46:00 -0500

    installed and used useSWR in last-sales to fetch data

--- sections/002-data-fetching/data-fetching/pages/last-sales.js
+++ sections/002-data-fetching/data-fetching/pages/last-sales.js
@@ -1,29 +1,53 @@
 import { useEffect, useState } from "react";
 import { Route } from "../data/config";
+import useSWR from "swr";
+
 export default function LastSalesPage() {
   const [sales, setSales] = useState([]);
-  const [loading, setIsLoading] = useState(true);
+  //   const [loading, setIsLoading] = useState(true);
+
+  const { data, error } = useSWR(
+    "https://nextjs-demo-8bed5-default-rtdb.firebaseio.com/sales.json",
+    (url) => fetch(url).then((res) => res.json())
+  );
 
   useEffect(() => {
-    fetch(`https://nextjs-demo-8bed5-default-rtdb.firebaseio.com/sales.json`)
-      .then((res) => res.json())
-      .then((data) => {
-        const newSales = [];
+    if (data) {
+      const newSales = [];
+      for (const key in data) {
+        newSales.push({
+          key: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+      setSales(newSales);
+    }
+  }, [data]);
+  //   useEffect(() => {
+  //     fetch(`https://nextjs-demo-8bed5-default-rtdb.firebaseio.com/sales.json`)
+  //       .then((res) => res.json())
+  //       .then((data) => {
+  //         const newSales = [];
 
-        for (const key in data) {
-          newSales.push({
-            key: key,
-            username: data[key].username,
-            volume: data[key].volume,
-          });
-        }
-        console.log(newSales);
-        setSales(newSales);
-        setIsLoading(false);
-      });
-  }, []);
+  //         for (const key in data) {
+  //           newSales.push({
+  //             key: key,
+  //             username: data[key].username,
+  //             volume: data[key].volume,
+  //           });
+  //         }
+  //         console.log(newSales);
+  //         setSales(newSales);
+  //         setIsLoading(false);
+  //       });
+  //   }, []);
 
-  if (loading) {
+  if (error) {
+    return <p>Failed to load</p>;
+  }
+
+  if (!sales || !data) {
     return (
       <div>
         <h1>Loading...</h1>
@@ -46,4 +70,10 @@ export default function LastSalesPage() {
 /* Notes:
 - Sometimes fetching data on the client side is necessary, which can be done the standard way it would be done in React, by fetching the data in
 a use effect hook.
+
+- a useful third-party hook is the useSWR hook, which at its core allows you to achieve data-fetching in a simpler way. Instead of implementing
+your own logic to fetch in useEffect, simply pass in the url you'd like to get data from into useSWR, and deconstruct its returned object into
+the values {data, error}, which can be treated the same as the response you'd get from using fetch().
+-a note: in this lecture I had to add "(url) => fetch(url).then((res) => res.json())" as a second argument to useSWR in order to properly fetch the
+data, otherwise there will be no data returned and oddly with no errors.
 */

e6c18eef2856e5370df86c0ccf2d786a1e8b55bf
JP Eiler <jp.eiler97@gmail.com>
2022-07-24 08:57:35 -0500

    combined client side and server side data fetching

--- sections/002-data-fetching/data-fetching/pages/last-sales.js
+++ sections/002-data-fetching/data-fetching/pages/last-sales.js
@@ -47,7 +47,7 @@ export default function LastSalesPage() {
     return <p>Failed to load</p>;
   }
 
-  if (!sales || !data) {
+  if (!sales && !data) {
     return (
       <div>
         <h1>Loading...</h1>
@@ -67,6 +67,25 @@ export default function LastSalesPage() {
   );
 }
 
+export async function getStaticProps(props) {
+  return fetch(
+    `https://nextjs-demo-8bed5-default-rtdb.firebaseio.com/sales.json`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      const newSales = [];
+
+      for (const key in data) {
+        newSales.push({
+          key: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+
+      return { props: { sales: transformedSales }, revalidate: 10 };
+    });
+}
 /* Notes:
 - Sometimes fetching data on the client side is necessary, which can be done the standard way it would be done in React, by fetching the data in
 a use effect hook.
@@ -74,6 +93,10 @@ a use effect hook.
 - a useful third-party hook is the useSWR hook, which at its core allows you to achieve data-fetching in a simpler way. Instead of implementing
 your own logic to fetch in useEffect, simply pass in the url you'd like to get data from into useSWR, and deconstruct its returned object into
 the values {data, error}, which can be treated the same as the response you'd get from using fetch().
+
 -a note: in this lecture I had to add "(url) => fetch(url).then((res) => res.json())" as a second argument to useSWR in order to properly fetch the
 data, otherwise there will be no data returned and oddly with no errors.
+
+-client side and server side rendering can be used in unison. By making a call for the same data in both getStaticProps and the useSWR hook,
+we can both ensure that data is pre-fetched to view in the source (getStaticProps), and also re-fetched when it is updated (useSWR)
 */

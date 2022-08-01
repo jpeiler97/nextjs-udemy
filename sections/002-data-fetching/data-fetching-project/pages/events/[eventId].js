c2303c1a44d3b2fdea0b004046a29e2db30d205c
JP Eiler <jp.eiler97@gmail.com>
2022-08-01 08:06:27 -0500

    set up back end data for project

--- sections/002-data-fetching/data-fetching-project/pages/events/[eventId].js
+++ sections/002-data-fetching/data-fetching-project/pages/events/[eventId].js
@@ -1,17 +1,13 @@
-import { Fragment } from 'react';
-import { useRouter } from 'next/router';
+import { Fragment } from "react";
 
-import { getEventById } from '../../dummy-data';
-import EventSummary from '../../components/event-detail/event-summary';
-import EventLogistics from '../../components/event-detail/event-logistics';
-import EventContent from '../../components/event-detail/event-content';
-import ErrorAlert from '../../components/ui/error-alert';
+import { getAllEvents, getEventById } from "../../helpers/apiUtil";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+import ErrorAlert from "../../components/ui/error-alert";
 
-function EventDetailPage() {
-  const router = useRouter();
-
-  const eventId = router.query.eventId;
-  const event = getEventById(eventId);
+function EventDetailPage(props) {
+  const { event } = props;
 
   if (!event) {
     return (
@@ -35,6 +31,30 @@ function EventDetailPage() {
       </EventContent>
     </Fragment>
   );
+}
+
+export async function getStaticProps(context) {
+  const eventId = context.params.eventId;
+  const event = await getEventById(eventId);
+
+  return {
+    props: {
+      event: event,
+    },
+  };
+}
+
+export async function getStaticPaths(context) {
+  const events = await getAllEvents();
+
+  const ids = events.map((e) => {
+    return { params: { eventId: e.id } };
+  });
+
+  return {
+    paths: ids,
+    fallback: true,
+  };
 }
 
 export default EventDetailPage;
--- sections/002-data-fetching/data-fetching-project/pages/index.js
+++ sections/002-data-fetching/data-fetching-project/pages/index.js
@@ -1,14 +1,22 @@
-import { getFeaturedEvents } from '../dummy-data';
-import EventList from '../components/events/event-list';
+import EventList from "../components/events/event-list";
+import { getFeaturedEvents } from "../helpers/apiUtil";
 
-function HomePage() {
-  const featuredEvents = getFeaturedEvents();
-
+function HomePage(props) {
   return (
     <div>
-      <EventList items={featuredEvents} />
+      <EventList items={props.featuredEvents} />
     </div>
   );
 }
 
 export default HomePage;
+
+export async function getStaticProps(context) {
+  let events = await getFeaturedEvents();
+
+  return {
+    props: {
+      featuredEvents: events,
+    },
+  };
+}

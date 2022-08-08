cb9540712e26cfce1e20b6e9fa1e4a6d56dc87f7
JP Eiler <jp.eiler97@gmail.com>
2022-08-08 10:43:45 -0500

    added notes about conflicting Head components

--- sections/003-optimizing-nextjs/optimizing-nextjs/pages/_app.js
+++ sections/003-optimizing-nextjs/optimizing-nextjs/pages/_app.js
@@ -21,4 +21,11 @@ export default MyApp;
   pages rendered by the app. This can be useful if trying to avoid repeating the Head component
   when you need a setting that is global to the entire app, not just one/a handful of pages.
 
+  - When adding a Head to the _app.js and also to multiple specific pages, Next merges the content of these
+  components. In the case of conflicting elements, the latter will always be chosen. That is, the most "recent"
+  line. In the same file, a <title> on line 26 would be chosen over line 25. In different files, the <title> in
+  a child component would be chosen over the one in _app.js.
+
+  - a situation where such a conflict might be useful is to have a general page title and description that entails
+  all pages by default, until that page is specifically given a title and description.
 */

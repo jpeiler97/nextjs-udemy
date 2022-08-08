f08790b6e7dcd4899856ef4dc8615c2153cb1c24
JP Eiler <jp.eiler97@gmail.com>
2022-08-08 10:36:49 -0500

    added Head component to _app.js

--- sections/003-optimizing-nextjs/optimizing-nextjs/pages/_app.js
+++ sections/003-optimizing-nextjs/optimizing-nextjs/pages/_app.js
@@ -1,12 +1,24 @@
-import Layout from '../components/layout/layout';
-import '../styles/globals.css';
+import Head from "next/head";
+
+import Layout from "../components/layout/layout";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
 }
 
 export default MyApp;
+
+/* Notes:
+  - By adding a Head component to _app.js, we are able to add Head settings to all
+  pages rendered by the app. This can be useful if trying to avoid repeating the Head component
+  when you need a setting that is global to the entire app, not just one/a handful of pages.
+
+*/
